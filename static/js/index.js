f9c960ea397efae112ac1b540a596db57c9e5890
syeeuns <syeeuns@gmail.com>
2021-03-15 00:13:18 +0900

    private chat added

--- static/js/index.js
+++ static/js/index.js
@@ -5,10 +5,11 @@ var socket = io()
 // 접속되었을 때 실행
 socket.on('connect', function () {
     var name = prompt('반갑습니다!', '')
-    if (!name) {
+    if (!name)
         name = '익명'
-    }
+
     socket.emit('newUser', name)
+
 })
 
 socket.on('update', function (data) {
@@ -37,8 +38,10 @@ socket.on('update', function (data) {
 function send() {
     // 창에 입력된 데이터 가져오기
     var message = document.getElementById('test').value
+    var username = document.getElementById('whisper').value
     // 가져왔으니 데이터창 비워주기
     document.getElementById('test').value = ''
+    document.getElementById('whisper').value = ''
     var chat = document.getElementById('chat')
     var msg = document.createElement('div')
     var node = document.createTextNode(message)
@@ -46,7 +49,7 @@ function send() {
     msg.appendChild(node)
     chat.appendChild(msg)
     // 서버로 sent 이벤트 전달 + 데이터 같이
-    socket.emit('message', { type: 'message', message: message })
+    socket.emit('message', { type: 'message', message: message, username: username })
     //on은 수신, emit은 전송
     // send라는 이름의 이벤트를 전송했으면 받는곳에서는 on('send') 가 있어야 받을 수 있음
     // 이벤트명이 동일한것 끼리만 데이터 송/수신이 가능
